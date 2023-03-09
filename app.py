9bb5fa26bef238753d8a80cfcf1f0e76a91a9afa
hashi1102 <89903471+hashi1102@users.noreply.github.com>
2023-03-09 16:08:04 +0900

    Update app.py

--- app.py
+++ app.py
@@ -1,19 +1,47 @@
 import streamlit as st
-from streamlit_webrtc import webrtc_streamer
 import cv2
-import av #strealing video library
+import time
+from PIL import Image
+from model import predict
 
-st.title('Streamlit App Test')
-st.write('Gray Scale')
 
-#Class
-class VideoProcessor:
-    def recv(self,frame):
 
-        img = frame.to_ndarray(format = 'bgr24')
-        img = cv2.cvtColor(img, cv2.COLOR_RGB2GRAY)
-        img = av.VideoFrame.from_ndarray(img, format='gray')
+st.markdown("# Camera Application")
 
-        return img
+device = '0'
+with st.spinner():
+    if device.isnumeric():
+        device = int(device)
+    cap = cv2.VideoCapture(device)
 
-webrtc_streamer(key='example', video_processor_factory=VideoProcessor)
+    image_loc = st.empty()
+    with st.empty():
+        while cap.isOpened:
+            ret, img = cap.read()
+            time.sleep(1)
+            img = Image.fromarray(cv2.cvtColor(img, cv2.COLOR_BGR2RGB))
+            image_loc.image(img)
+
+            if img is not None:
+
+                # # 予測
+                results = predict(img)
+
+                # 結果の表示
+                n_top = 3  # 確率が高い順に3位まで返す
+                for result in results[:n_top]:
+                    r = "判定結果 : " + str(round(result[2]*100, 2)) + "%の確率で" + result[0] + "です。"
+                    st.write(f'{r}')
+                if cv2.waitKey(1) & 0xFF == ord("q"):
+                    break
+
+        cap.release()
+
+
+hide_streamlit_style = """
+            <style>
+            #MainMenu {visibility: hidden;}
+            footer {visibility: hidden;}
+            </style>
+            """
+st.markdown(hide_streamlit_style, unsafe_allow_html=True)
