cf6441c76d84db5002d48048e42006fee2586d50
hashi1102 <89903471+hashi1102@users.noreply.github.com>
2023-03-09 22:06:38 +0900

    Update app.py

--- app.py
+++ app.py
@@ -5,3 +5,12 @@ st.title("My first Streamlit app")
 st.write("Hello, world")
 
 webrtc_streamer(key="example")
+
+
+webrtc_streamer(
+    key="example",
+    video_frame_callback=callback,
+    rtc_configuration={  # この設定を足す
+        "iceServers": [{"urls": ["stun:stun.l.google.com:19302"]}]
+    }
+)
