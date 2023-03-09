0310811d3b265ab98cbe2a91965c6780f2ed936d
hashi1102 <89903471+hashi1102@users.noreply.github.com>
2023-03-09 18:15:22 +0900

    Update app.py

--- app.py
+++ app.py
@@ -12,7 +12,7 @@ device = 0
 with st.spinner():
 #     if device.isnumeric():
 #         device = int(device)
-    cap = cv2.VideoCapture(device, cv2.CAP_DSHOW)
+    cap = cv2.VideoCapture(device)
 
     image_loc = st.empty()
     with st.empty():
