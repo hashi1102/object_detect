7d0a4714d26c86c58bfad8c86b7487d8ddedd510
hashi1102 <89903471+hashi1102@users.noreply.github.com>
2023-03-09 14:10:05 +0900

    Add files via upload

---
+++ app.py
@@ -0,0 +1,51 @@
+import streamlit as st
+import cv2
+import time
+from PIL import Image
+from model import predict
+
+
+def main():
+    st.markdown("# Camera Application")
+
+    device = '0'
+    with st.spinner():
+        if device.isnumeric():
+            device = int(device)
+        cap = cv2.VideoCapture(device)
+
+        image_loc = st.empty()
+        with st.empty():
+            while cap.isOpened:
+                ret, img = cap.read()
+                time.sleep(1)
+                img = Image.fromarray(cv2.cvtColor(img, cv2.COLOR_BGR2RGB))
+                image_loc.image(img)
+
+                if img is not None:
+
+                    # # 予測
+                    results = predict(img)
+
+                    # 結果の表示
+                    n_top = 3  # 確率が高い順に3位まで返す
+                    for result in results[:n_top]:
+                        r = "判定結果 : " + str(round(result[2]*100, 2)) + "%の確率で" + result[0] + "です。"
+                        st.write(f'{r}')
+                    if cv2.waitKey(1) & 0xFF == ord("q"):
+                        break
+
+            cap.release()
+
+
+hide_streamlit_style = """
+            <style>
+            #MainMenu {visibility: hidden;}
+            footer {visibility: hidden;}
+            </style>
+            """
+st.markdown(hide_streamlit_style, unsafe_allow_html=True) 
+
+
+if __name__ == "__main__":
+    main()
