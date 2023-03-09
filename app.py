eb8c498b11f1e470ba581168d413c4d3401eb0c1
hashi1102 <89903471+hashi1102@users.noreply.github.com>
2023-03-09 21:10:20 +0900

    Update app.py

--- app.py
+++ app.py
@@ -22,19 +22,21 @@ def callback(frame):
 #                 img = Image.fromarray(cv2.cvtColor(img, cv2.COLOR_BGR2RGB))
 #                 image_loc.image(img)
 
-            if img is not None:
+#             if img is not None:
 
-                # # 予測
-                results = predict(img)
+            # # 予測
+            results = predict(img)
 
-                # 結果の表示
-                n_top = 3  # 確率が高い順に3位まで返す
-                for result in results[:n_top]:
-                    r = "判定結果 : " + str(round(result[2]*100, 2)) + "%の確率で" + result[0] + "です。"
-                    st.write(f'{r}')
+            # 結果の表示
+            n_top = 3  # 確率が高い順に3位まで返す
+            for result in results[:n_top]:
+                r = "判定結果 : " + str(round(result[2]*100, 2)) + "%の確率で" + result[0] + "です。"
+                st.write(f'{r}')
 #                 if cv2.waitKey(1) & 0xFF == ord("q"):
 #                     break
 
+
+webrtc_streamer(key="example", video_frame_callback=callback)
 
 
 
@@ -45,8 +47,3 @@ hide_streamlit_style = """
             </style>
             """
 st.markdown(hide_streamlit_style, unsafe_allow_html=True)
-    
-    
-
-
-webrtc_streamer(key="example", video_frame_callback=callback)
