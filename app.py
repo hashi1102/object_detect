c3e7e36b32b189a7733432972ce45797b791f48a
hashi1102 <89903471+hashi1102@users.noreply.github.com>
2023-03-09 22:20:49 +0900

    Update app.py

--- app.py
+++ app.py
@@ -1,27 +1,47 @@
 import streamlit as st
-from streamlit_webrtc import webrtc_streamer
-import av
 import cv2
-
-st.title("My first Streamlit app")
-st.write("Hello, world")
-
-threshold1 = st.slider("Threshold1", min_value=0, max_value=1000, step=1, value=100)
-threshold2 = st.slider("Threshold2", min_value=0, max_value=1000, step=1, value=200)
+import time
+from PIL import Image
+from model import predict
 
 
-def callback(frame):
-    img = frame.to_ndarray(format="bgr24")
 
-    img = cv2.cvtColor(cv2.Canny(img, threshold1, threshold2), cv2.COLOR_GRAY2BGR)
+st.markdown("# Camera Application")
 
-    return av.VideoFrame.from_ndarray(img, format="bgr24")
+device = '0'
+with st.spinner():
+    if device.isnumeric():
+        device = int(device)
+    cap = cv2.VideoCapture(device)
+
+    image_loc = st.empty()
+    with st.empty():
+        while cap.isOpened:
+            _, img = cap.read()
+            time.sleep(1)
+            img = Image.fromarray(cv2.cvtColor(img, cv2.COLOR_BGR2RGB))
+            image_loc.image(img)
+
+            if img is not None:
+
+                # # 予測
+                results = predict(img)
+
+                # 結果の表示
+                n_top = 3  # 確率が高い順に3位まで返す
+                for result in results[:n_top]:
+                    r = "判定結果 : " + str(round(result[2]*100, 2)) + "%の確率で" + result[0] + "です。"
+                    st.write(f'{r}')
+                if cv2.waitKey(1) & 0xFF == ord("q"):
+                    break
+
+        cap.release()
 
 
-webrtc_streamer(
-    key="example",
-    video_frame_callback=callback,
-    rtc_configuration={  # Add this line
-        "iceServers": [{"urls": ["stun:stun.l.google.com:19302"]}]
-    }
-)
+hide_streamlit_style = """
+            <style>
+            #MainMenu {visibility: hidden;}
+            footer {visibility: hidden;}
+            </style>
+            """
+st.markdown(hide_streamlit_style, unsafe_allow_html=True)
