35ede2ad3ad1caa36952a39c1c125f13baf19a8e
hashi1102 <89903471+hashi1102@users.noreply.github.com>
2023-03-09 15:19:46 +0900

    Update app.py

--- app.py
+++ app.py
@@ -1,7 +1,7 @@
 import streamlit as st
 import cv2
 import time
-import Image
+from PIL import Image
 from model import predict
 
 
@@ -14,34 +14,34 @@ with st.spinner():
         device = int(device)
     cap = cv2.VideoCapture(device)
 
-    image_loc = st.empty()
-    with st.empty():
-        while cap.isOpened:
-            ret, img = cap.read()
-            time.sleep(2)
-            img = Image.fromarray(cv2.cvtColor(img, cv2.COLOR_BGR2RGB))
-            image_loc.image(img)
+#     image_loc = st.empty()
+#     with st.empty():
+#         while cap.isOpened:
+#             ret, img = cap.read()
+#             time.sleep(2)
+#             img = Image.fromarray(cv2.cvtColor(img, cv2.COLOR_BGR2RGB))
+#             image_loc.image(img)
 
-            if img is not None:
+#             if img is not None:
 
-                # # 予測
-                results = predict(img)
+#                 # # 予測
+#                 results = predict(img)
 
-                # 結果の表示
-                n_top = 3  # 確率が高い順に3位まで返す
-                for result in results[:n_top]:
-                    r = "判定結果 : " + str(round(result[2]*100, 2)) + "%の確率で" + result[0] + "です。"
-                    st.write(f'{r}')
-                if cv2.waitKey(1) & 0xFF == ord("q"):
-                    break
+#                 # 結果の表示
+#                 n_top = 3  # 確率が高い順に3位まで返す
+#                 for result in results[:n_top]:
+#                     r = "判定結果 : " + str(round(result[2]*100, 2)) + "%の確率で" + result[0] + "です。"
+#                     st.write(f'{r}')
+#                 if cv2.waitKey(1) & 0xFF == ord("q"):
+#                     break
 
-        cap.release()
+#         cap.release()
 
 
-hide_streamlit_style = """
-            <style>
-            #MainMenu {visibility: hidden;}
-            footer {visibility: hidden;}
-            </style>
-            """
-st.markdown(hide_streamlit_style, unsafe_allow_html=True)
+# hide_streamlit_style = """
+#             <style>
+#             #MainMenu {visibility: hidden;}
+#             footer {visibility: hidden;}
+#             </style>
+#             """
+# st.markdown(hide_streamlit_style, unsafe_allow_html=True)
