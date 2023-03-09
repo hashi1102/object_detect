54d5e4b69385c7f7cded48ade4a01bb0819290c7
hashi1102 <89903471+hashi1102@users.noreply.github.com>
2023-03-09 22:09:47 +0900

    Update app.py

--- app.py
+++ app.py
@@ -4,6 +4,13 @@ from streamlit_webrtc import webrtc_streamer
 st.title("My first Streamlit app")
 st.write("Hello, world")
 
+def callback(frame):
+    img = frame.to_ndarray(format="bgr24")
+
+    img = cv2.cvtColor(cv2.Canny(img, threshold1, threshold2), cv2.COLOR_GRAY2BGR)
+
+    return av.VideoFrame.from_ndarray(img, format="bgr24")
+
 
 webrtc_streamer(
     key="example",
