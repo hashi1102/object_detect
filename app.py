11304b6b2b56e1e4f76aa9fd5b166cf4422d77e4
hashi1102 <89903471+hashi1102@users.noreply.github.com>
2023-03-10 13:51:42 +0900

    Update app.py

--- app.py
+++ app.py
@@ -1,32 +1,18 @@
 import streamlit as st
-from streamlit_webrtc import webrtc_streamer
-import av
 import cv2
+import numpy as np
 
-st.title("My first Streamlit app")
-st.write("Hello, world")
+img_file_buffer = st.camera_input("Take a picture")
 
+if img_file_buffer is not None:
+    # To read image file buffer with OpenCV:
+    bytes_data = img_file_buffer.getvalue()
+    cv2_img = cv2.imdecode(np.frombuffer(bytes_data, np.uint8), cv2.IMREAD_COLOR)
 
-class VideoProcessor:
-    def __init__(self) -> None:
-        self.threshold1 = 100
-        self.threshold2 = 200
+    # Check the type of cv2_img:
+    # Should output: <class 'numpy.ndarray'>
+    st.write(type(cv2_img))
 
-    def recv(self, frame):
-        img = frame.to_ndarray(format="bgr24")
-
-        img = cv2.cvtColor(cv2.Canny(img, self.threshold1, self.threshold2), cv2.COLOR_GRAY2BGR)
-
-        return av.VideoFrame.from_ndarray(img, format="bgr24")
-
-
-ctx = webrtc_streamer(
-    key="example",
-    video_processor_factory=VideoProcessor,
-    rtc_configuration={
-        "iceServers": [{"urls": ["stun:stun.l.google.com:19302"]}]
-    }
-)
-if ctx.video_processor:
-    ctx.video_processor.threshold1 = st.slider("Threshold1", min_value=0, max_value=1000, step=1, value=100)
-    ctx.video_processor.threshold2 = st.slider("Threshold2", min_value=0, max_value=1000, step=1, value=200)
+    # Check the shape of cv2_img:
+    # Should output shape: (height, width, channels)
+    st.write(cv2_img.shape)
