cef4a57f23de479aa33b0ecc29b2f570fbfa651b
hashi1102 <89903471+hashi1102@users.noreply.github.com>
2023-03-11 23:12:52 +0900

    Update app.py

--- app.py
+++ app.py
@@ -1,23 +1,42 @@
+# import streamlit as st
+# import cv2
+# import time
+# from PIL import Image
+# from model import predict
+# import numpy as np
+
+
+# img_file_buffer = st.camera_input("Take a picture")
+
+
+# if img_file_buffer is not None:
+#     # To read image file buffer with OpenCV:
+#     bytes_data = img_file_buffer.getvalue()
+#     cv2_img = cv2.imdecode(np.frombuffer(bytes_data, np.uint8), cv2.IMREAD_COLOR)
+
+#     # Check the type of cv2_img:
+#     # Should output: <class 'numpy.ndarray'>
+#     st.write(type(cv2_img))
+
+#     # Check the shape of cv2_img:
+#     # Should output shape: (height, width, channels)
+#     st.write(cv2_img.shape)
+
 import streamlit as st
+from streamlit_webrtc import webrtc_streamer
+import av
 import cv2
-import time
-from PIL import Image
-from model import predict
-import numpy as np
+
+st.title("My first Streamlit app")
+st.write("Hello, world")
 
 
-img_file_buffer = st.camera_input("Take a picture")
+def callback(frame):
+    img = frame.to_ndarray(format="bgr24")
+
+    img = cv2.cvtColor(cv2.Canny(img, 100, 200), cv2.COLOR_GRAY2BGR)
+
+    return av.VideoFrame.from_ndarray(img, format="bgr24")
 
 
-if img_file_buffer is not None:
-    # To read image file buffer with OpenCV:
-    bytes_data = img_file_buffer.getvalue()
-    cv2_img = cv2.imdecode(np.frombuffer(bytes_data, np.uint8), cv2.IMREAD_COLOR)
-
-    # Check the type of cv2_img:
-    # Should output: <class 'numpy.ndarray'>
-    st.write(type(cv2_img))
-
-    # Check the shape of cv2_img:
-    # Should output shape: (height, width, channels)
-    st.write(cv2_img.shape)
+webrtc_streamer(key="example", video_frame_callback=callback)
