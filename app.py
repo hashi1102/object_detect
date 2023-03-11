38ce5c6f1ba1304188360f6f21f863d20c4c0676
hashi1102 <89903471+hashi1102@users.noreply.github.com>
2023-03-12 00:32:01 +0900

    Update app.py

--- app.py
+++ app.py
@@ -24,34 +24,10 @@
 
 import streamlit as st
 from streamlit_webrtc import webrtc_streamer
-import av
 import cv2
+import av #strealing video library
 
-st.title("My first Streamlit app")
-st.write("Hello, world")
+st.title('Streamlit App Test')
+st.write('Hello world')
 
-threshold1 = st.slider("Threshold1", min_value=0, max_value=1000, step=1, value=100)
-threshold2 = st.slider("Threshold2", min_value=0, max_value=1000, step=1, value=200)
-
-
-def callback(frame):
-    cap = cv2.VideoCapture(0)
-    with st.empty():
-        while cap.isOpened:
-            _, img = cap.read()
-            time.sleep(1)
-            img = Image.fromarray(cv2.cvtColor(img, cv2.COLOR_BGR2RGB))
-
-
-
-        return image_loc.image(img)
-    cap.release()
-
-
-webrtc_streamer(
-    key="example",
-    video_frame_callback=callback,
-    rtc_configuration={  # Add this line
-        "iceServers": [{"urls": ["stun:stun.l.google.com:19302"]}]
-    }
-)
+webrtc_streamer(key='example')
