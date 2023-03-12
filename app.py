a36c13605ee86a866bf1312d8082e7ff125d40fe
hashi1102 <89903471+hashi1102@users.noreply.github.com>
2023-03-12 19:38:06 +0900

    Update app.py

--- app.py
+++ app.py
@@ -7,14 +7,15 @@ cap = cv.VideoCapture(0)
 frameST = st.empty()
 
 while True: 
-    ret, frame = cap.read()
-    # Stop the program if reached end of video
-    if not ret:
-        print("Done processing !!!")
-        #cv.waitKey(3000)
-        # Release device
-        cap.release()
-        break
+    with st.spinner():
+        ret, frame = cap.read()
+        # Stop the program if reached end of video
+        if not ret:
+            print("Done processing !!!")
+            #cv.waitKey(3000)
+            # Release device
+            cap.release()
+            break
 
-    #frameST.image(frame, channels="BGR")
-    image_loc.image(frame)
+        #frameST.image(frame, channels="BGR")
+        image_loc.image(frame)
