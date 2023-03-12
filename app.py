b39c8a1bba1bde2a6d649eda97b099e6ca95fac8
hashi1102 <89903471+hashi1102@users.noreply.github.com>
2023-03-12 09:07:09 +0900

    Update app.py

--- app.py
+++ app.py
@@ -1,6 +1,6 @@
 import streamlit as st
 
-picture = st.camera_input("Take a picture", disabled=True)
+picture = st.camera_input("Take a picture", on_change=None, args=None, kwargs=True)
 
 if picture:
     st.image(picture)
