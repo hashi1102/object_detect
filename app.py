581e633fb377d76d9e9161f68444ba5e3676f630
hashi1102 <89903471+hashi1102@users.noreply.github.com>
2023-03-12 19:43:45 +0900

    Update app.py

--- app.py
+++ app.py
@@ -9,8 +9,6 @@ frameST = st.empty()
 with st.spinner():
     while True: 
         ret, frame = cap.read()
-        #img = Image.fromarray(cv.cvtColor(frame, cv2.COLOR_BGR2RGB))
-        st.image(frame)
         # Stop the program if reached end of video
         if not ret:
             print("Done processing !!!")
