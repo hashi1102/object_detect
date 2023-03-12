b91baadeba74590eecd69b729b0a10124eaae04f
hashi1102 <89903471+hashi1102@users.noreply.github.com>
2023-03-12 11:09:19 +0900

    Update app.py

--- app.py
+++ app.py
@@ -1,6 +1,20 @@
 import streamlit as st
+import torch
+import numpy as np
 
-picture = st.camera_input("Take a picture", help="aaa")
+img_file_buffer = st.camera_input("Take a picture")
 
-if picture:
-    st.image(picture)
+if img_file_buffer is not None:
+    # To read image file buffer as a 3D uint8 tensor with PyTorch:
+    bytes_data = img_file_buffer.getvalue()
+    torch_img = torch.ops.image.decode_image(
+        torch.from_numpy(np.frombuffer(bytes_data, np.uint8)), 3
+    )
+
+    # Check the type of torch_img:
+    # Should output: <class 'torch.Tensor'>
+    st.write(type(torch_img))
+
+    # Check the shape of torch_img:
+    # Should output shape: torch.Size([channels, height, width])
+    st.write(torch_img.shape)
