e6608cf8583ae2285d3aae50d36baec141857c58
hashi1102 <89903471+hashi1102@users.noreply.github.com>
2023-03-12 16:34:17 +0900

    Update app.py

--- app.py
+++ app.py
@@ -1,38 +1,19 @@
 import streamlit as st
-from threading import Thread
-import cv2
-import time
+import cv2 as cv
 
-cap = cv2.VideoCapture(0)
+cap = cv.VideoCapture(0)
+
 frameST = st.empty()
-param = st.sidebar.slider('choose your value')
+param=st.sidebar.slider('chose your value')
 
-stopper_started = False
-while True:
-    success, frame = cap.read()
-    if not success: break
+while True: 
+    ret, frame = cap.read()
+    # Stop the program if reached end of video
+    if not ret:
+        print("Done processing !!!")
+        cv.waitKey(3000)
+        # Release device
+        cap.release()
+        break
 
     frameST.image(frame, channels="BGR")
-    updated_time = time.time()
-
-    if not stopper_started:
-        #this block executes for once with every
-        #streamlit re-run command
-        def stopper(self):
-            while True:
-                try:
-                    #if time difference increases this thread will be terminated
-                    time_diff = round(time.time() - updated_time)
-                    if time_diff >1: #1 second
-                        print("Done processing !!!")
-                        print("Releasing VideoCapture")
-                        #if streamlit ui is stopped
-                        #time gap will increase
-                        #hence releasing camera resource
-                        cap.release()
-                        break
-                except: pass
-        th = Thread(target=stopper, args=(0,))
-        th.daemon = True
-        th.start()
-        stopper_started = True
