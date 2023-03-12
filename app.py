193f4b8eeb757c2b1a97aa77de49126716a9c14b
hashi1102 <89903471+hashi1102@users.noreply.github.com>
2023-03-12 20:01:54 +0900

    Update app.py

--- app.py
+++ app.py
@@ -4,4 +4,10 @@ from streamlit_webrtc import webrtc_streamer
 st.title("My first Streamlit app")
 st.write("Hello, world")
 
-webrtc_streamer(key="example")
+webrtc_streamer(
+    key="example",
+    video_frame_callback=callback,
+    rtc_configuration={  # この設定を足す
+        "iceServers": [{"urls": ["stun:stun.l.google.com:19302"]}]
+    }
+)
